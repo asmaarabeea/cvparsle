e8908fa2e152402d0183ab54c1dcbc5904395049
asmaa <asmaa.rabeea7@gmail.com>
2018-05-16 02:18:36 -0700

    init

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+use Illuminate\Http\Request;
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    public function index()
+    {
+        return view('index'); 
+    }
+
+    public function parse(Request $request)
+    {
+        ini_set('default_socket_timeout', 600);
+        // $cvs = [url('/cv/1.pdf'), url('/cv/2.pdf')];
+        //$this->print_log("CV number #1", 2000);   
+        $cvs = $request->file('cvs');
+
+        $output = "";
+
+        if ($request->hasFile('cvs')) {
+
+            set_time_limit(0);
+            ob_implicit_flush(true);
+            ob_end_flush();
+            // ob_start();
+            foreach ($cvs as $key => $cv) {
+
+                $output = \CV::parse($cv);
+
+                $response = $cv->getClientOriginalName();
+                // echo "<br> Line to show.".$response."\n";  
+                $response = array('message' => $response . ' complete', 'progress' => 200);
+                echo json_encode($response);
+                // ob_flush();
+                // flush();
+                // sleep(1);
+            }
+
+          // echo "Done.";
+          // ob_end_flush();
+            $response = array('message' => 'Complete', 'progress' => 100);
+            echo json_encode($response);
+    
+         // return response()->json($output, 200);
+
+        }
+
+    }
+
+   
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
+
+</head>
+<body>
+<table width="600">
+	<form action="{{ url('/parse') }}" method="post" id="my_form" enctype="multipart/form-data">
+
+	<tr>
+	<td width="20%">Select file</td>
+	<td width="80%"><input type="file" name="cvs[]" id="cvs" multiple /></td>
+	</tr>
+
+	<tr>
+	<td>Submit</td>
+	<td><input type="submit" name="submit"  value="Submit" /></td>
+	</tr>
+
+	</form>
+</table>
+ <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="70"
+             aria-valuemin="0" aria-valuemax="100" style="width:0">
+            <span id="fullResponse"></span>
+        </div>
+    </div>
+    <h4 id="progressTest"></h4>
+<!-- <div id="results"></div> -->
+</body>
+</html>
+
+<script type="text/javascript">
+
+	$("#my_form").submit(function(event){
+
+		var lastResponseLength = false;
+
+	    event.preventDefault(); //prevent default action 
+	    var post_url = $(this).attr("action"); //get form action url
+	    var request_method = $(this).attr("method"); //get form GET/POST method
+	    var form_data = new FormData(this); //Creates new FormData object
+	    $.ajax({
+	        url : '/parse',
+	        type: request_method,
+	        data : form_data,
+	        contentType: false,
+	        cache: false,
+	        dataType: "json",
+	        processData:false,
+
+            xhrFields: {
+            // Getting on progress streaming response
+            onprogress: function(e)
+            {
+                var progressResponse;
+                var response = e.currentTarget.response;
+                if(lastResponseLength === false)
+                {
+                    progressResponse = response;
+                    lastResponseLength = response.length;
+                }
+                else
+                {
+                    progressResponse = response.substring(lastResponseLength);
+                    lastResponseLength = response.length;
+                }
+                var parsedResponse = JSON.parse(progressResponse);
+                $('#progressTest').text(progressResponse);
+                $('#fullResponse').text(parsedResponse.message);
+                $('.progress-bar').css('width', parsedResponse.progress + '%');
+            }
+
+            success: function(response) {
+                // $("#results").append(response);
+                console.log('Complete response = ' + response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        }
+
+	    // }).done(function(response){ //
+	    //     $("#results").html(response);
+
+	    });
+
+
+	});
+</script>
